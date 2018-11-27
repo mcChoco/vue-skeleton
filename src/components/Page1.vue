951bf38f8739532716530778ce85974e1a45593b
mcChoco <31406582+mcChoco@users.noreply.github.com>
2018-11-27 16:08:29 +0800

    demo
    verson 1.0.0

---
+++ src/components/Page1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Refresh current page, you'll see a different Skeleton!
+        <router-link to="/">Jump to Home{{a}}</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'page1',
+    data(){
+      return{
+        a:""
+      }
+    },
+    mounted() {
+      this.a = this.$route.params.type
+    },
+};
+</script>
